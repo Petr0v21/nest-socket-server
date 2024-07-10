30b4e550518a7afbcd37521aa8545daccdac1bcc
Петров Вячеслав <petr0v21vs@gmail.com>
2024-07-10 16:25:48 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,7 @@ import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.int
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const corsOptions: CorsOptions = {
-    origin: (origin, callback) => {
-      const allowedOrigins = [
-        'http://localhost:8080',
-        'http://localhost:3000',
-        'https://telegram-web-app-livid.vercel.app',
-      ];
-
-      if (allowedOrigins.includes(origin) || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-  };
-  app.enableCors(corsOptions);
+  app.enableCors();
   const port = Number(process.env.PORT ?? 3000);
   await app.listen(port);
   console.log('Server runing on port ' + port);
